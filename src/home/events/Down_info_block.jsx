b6cc66ba3c1e9eb019e27165501574434e86b873
Vyacheslav Savinkov <slava.savinkov1@mail.ru>
2025-01-28 15:19:59 +0500

    add event creator block

---
+++ src/home/events/Down_info_block.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Down_info_block = () => {
+    return (
+        <div className='down_info_block'>
+            eqfqef
+        </div>
+    );
+};
+
+export default Down_info_block;
--- src/home/events/Event.jsx
+++ src/home/events/Event.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import Event_bookmark from "./Event_bookmark";
 import Eventcreator from "./Eventcreator";
 import "./Event.css";
-const Event = ({ event_info}) => {
-    console.log(event_info)
+import Down_info_block from "./Down_info_block";
+const Event = ({ event_info }) => {
   return (
     <div>
-        <Eventcreator event_creator_name={event_info.event_creator_name} event_creator_image={event_info.event_creator_image}></Eventcreator>
       <div className="event_contrainer">
         <div
           className="background_event"
           style={{ backgroundImage: `url(${event_info.image_background})` }}
         >
-          <Event_bookmark></Event_bookmark>
+          <Event_bookmark />
         </div>
+        <div className="event_creator_block_overlay">
+          <Eventcreator
+            event_creator_name={event_info.event_creator_name}
+            event_creator_image={event_info.event_creator_image}
+          />
+        </div>
+      <Down_info_block></Down_info_block>
+        
       </div>
     </div>
   );
