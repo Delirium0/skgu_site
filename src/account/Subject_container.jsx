a7e44db8774a81f7290d1ae35293fcfa6dfcff84
Vyacheslav Savinkov <slava.savinkov1@mail.ru>
2025-04-01 14:35:29 +0500

    fix arrow and media

--- src/account/Subject_container.jsx
+++ src/account/Subject_container.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import cl from "./Subject_container.module.css";
-import { ReactComponent as ArrowDownSvg } from './../assets/svg/arrow_down.svg';
+import { ReactComponent as ArrowUpSvg } from './../assets/svg/arrow_down.svg'; // Предположим, что вы будете использовать эту же иконку, но визуально как стрелку вверх. Если у вас есть `arrow_up.svg`, используйте ее
 
 const Subject_container = () => {
   return (
     <div className={cl.subject_container}>
-      <div className={cl.card}>qefqef</div>
+      <div className={cl.card}>
+        <div className={cl.text_container}> {/* Добавим контейнер для текста */}
+          <div className={cl.subject_title}>Протоколы и интерфейсы компьютерных систем</div>
+          <div className={cl.teacher_name}>Ушакова Е.В.</div>
+        </div>
+       <div className={cl.arrow_list}>▼</div>
+      </div>
       <div className={cl.another_container}>
-        {/* Передаем нужный цвет через fill */}
-        <ArrowDownSvg className={cl.progress_icon} />
+        <ArrowUpSvg className={cl.progress_icon} /> {/* Используем стрелку в правом контейнере */}
+        <div className={cl.number_container}>100</div> {/* Добавим контейнер для числа */}
       </div>
     </div>
   );
