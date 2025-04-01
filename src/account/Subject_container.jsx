e9ee750909cd1efb19db48d2021ceede48702d97
Vyacheslav Savinkov <slava.savinkov1@mail.ru>
2025-04-01 15:55:11 +0500

    perenos

--- src/account/Subject_container.jsx
+++ src/account/Subject_container.jsx
@@ -2,16 +2,10 @@ import React, { useState } from "react";
 import cl from "./Subject_container.module.css";
 import { ReactComponent as ArrowUpSvg } from './../assets/svg/arrow_down.svg';
 
-const Subject_container = () => {
+const Subject_container = ({currentSubjectData}) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   // Данные для текущего предмета (жестко заданы для примера)
-  const currentSubjectData = {
-    'subject_name': 'Протоколы и интерфейсы компьютерных систем',
-    'teacher': 'Ушакова Екатерина Вячеславовна',
-    '1': '33', '2': '100|100|100', '3': '0', '4': '100|0', '5': '95|85|84', '6': '78', '7': '78', '8': '65', '9': '95', // Добавим оценки для 9-16 модулей для примера
-    '10': '94', '11': '96', '12': '-', '13': '97', '14': '91', '15': '95|85|84', '16': '88',
-    'average_grade': '98'
-  };
+
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -46,14 +40,14 @@ const Subject_container = () => {
     <div className={cl.subject_container} onClick={toggleDropdown}>
       <div className={cl.card}>
         <div className={cl.text_container}>
-          <div className={cl.subject_title}>Протоколы и интерфейсы компьютерных систем</div>
-          <div className={cl.teacher_name}>Ушакова Е.В.</div>
+          <div className={cl.subject_title}>{currentSubjectData.subject_name}</div>
+          <div className={cl.teacher_name}>{currentSubjectData.teacher}</div>
         </div>
        <div className={cl.arrow_list} style={{ transform: isDropdownOpen ? 'scaleY(-0.7)' : 'scaleY(0.7)' }}>▼</div>
       </div>
       <div className={cl.another_container}>
         <ArrowUpSvg className={cl.progress_icon} />
-        <div className={cl.number_container}>100</div>
+        <div className={cl.number_container}>{currentSubjectData.average_grade}</div> {/* Use average_grade here */}
       </div>
 
       {isDropdownOpen && (
